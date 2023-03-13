80ba289ef15eae7a64b589238adaa4b292bdde37
ksmagu <k.smagurauskaite@gmail.com>
2023-03-13 14:42:09 +0200

    added logic to buttons

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -6,21 +6,26 @@ import './home.scss';
 import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
-    const [selectedTopic, setSelectedTopic] = useState<string>('');
-    const navigate = useNavigate();
+  const [selectedTopic, setSelectedTopic] = useState<string>('');
+  const [activeButton, setActiveButton] = useState<string>('');
+  const navigate = useNavigate();
 
-    const handleClick = (event: React.FormEvent<Element>) => {
-        // Listening to click and setting topic of clicked button
-        const topic = event.currentTarget.textContent || '';
-        setSelectedTopic(topic);
-        console.log(`Selected topic: ${topic}`);
-    };
+  const handleClick = (event: React.FormEvent<Element>) => {
+    // Listening to click and setting topic of clicked button
+    const topic = event.currentTarget.textContent || '';
+    console.log(`Selected topic: ${topic}`);
 
-    const handleStartClick = () => {
-        // Navigate to the Questions page with the selected topic
-        navigate(`/questions?topic=${selectedTopic}`);
-        console.log(`Starting trivia for ${selectedTopic}`);
-    };
+    // Set the active button and topic
+    setActiveButton(topic);
+    setSelectedTopic(topic)
+  };
+
+  const handleStartClick = () => {
+    // Navigate to the Questions page with the selected topic
+    navigate(`/questions?topic=${selectedTopic}`);
+    console.log(`Starting trivia for ${selectedTopic}`);
+  };
+
 
     return (
         <>
@@ -39,19 +44,39 @@ const Home: React.FC = () => {
                         Let's get started! Choose a topic:
                     </h1>
                     <div className='topics'>
-                        <Button color='$orange' onClick={handleClick}>
+                        <Button
+                            color='$orange'
+                            onClick={handleClick}
+                            active={activeButton === 'HTML'}
+                        >
                             HTML
                         </Button>
-                        <Button color='$orange' onClick={handleClick}>
+                        <Button
+                            color='$orange'
+                            onClick={handleClick}
+                            active={activeButton === 'CSS'}
+                        >
                             CSS
                         </Button>
-                        <Button color='$orange' onClick={handleClick}>
+                        <Button
+                            color='$orange'
+                            onClick={handleClick}
+                            active={activeButton === 'React'}
+                        >
                             React
                         </Button>
-                        <Button color='$orange' onClick={handleClick}>
+                        <Button
+                            color='$orange'
+                            onClick={handleClick}
+                            active={activeButton === 'JavaScript'}
+                        >
                             JavaScript
                         </Button>
-                        <Button color='$orange' onClick={handleClick}>
+                        <Button
+                            color='$orange'
+                            onClick={handleClick}
+                            active={activeButton === 'I know it all'}
+                        >
                             I know it all
                         </Button>
                     </div>
